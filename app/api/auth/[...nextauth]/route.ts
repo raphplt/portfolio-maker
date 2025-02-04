cedf549daccf00666a177e7244447e4dcaf701cb
raphaeltkorp <raphael@tkorp.com>
2025-02-04 23:43:39 +0100

    feat: enhance NextAuth configuration with TypeScript types for better type safety and clarity

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import { JWT } from "next-auth/jwt";
+import { Session } from "next-auth";
 
 const authOptions = {
 	providers: [
@@ -18,14 +21,22 @@ const authOptions = {
 		}),
 	],
 	callbacks: {
-		async jwt({ token, account, profile }) {
+		async jwt({
+			token,
+			account,
+			profile,
+		}: {
+			token: JWT;
+			account: any;
+			profile?: any;
+		}) {
 			if (account) {
 				token.accessToken = account.access_token;
 				token.githubLogin = profile?.login;
 			}
 			return token;
 		},
-		async session({ session, token }) {
+		async session({ session, token }: { session: Session; token: JWT }) {
 			session.accessToken = token.accessToken as string;
 			session.user.login = token.githubLogin as string;
 
